b63eb87d1d4ec5bdf2b45952527b4150cdab227a
Iqbal Hossan <engiqbal1999@gmail.com>
2022-04-28 16:12:23 +0600

    connected the mongodb

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((error) => console.log(error));
 
 app.get("/", (req, res) => {
   res.send("backed connected");

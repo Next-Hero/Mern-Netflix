349598331aff982eda8bf1c5de98355133fa4044
Iqbal Hossan <engiqbal1999@gmail.com>
2022-05-13 12:05:41 +0600

    created the featured section and display on the home page after the navbar

--- netflix-client-side/src/Pages/Home/Home.js
+++ netflix-client-side/src/Pages/Home/Home.js
@@ -1,3 +1,4 @@
+import Featured from "../../Components/Featured/Featured";
 import Navbar from "../../Components/Navbar/Navbar";
 import "./Home.scss";
 
@@ -5,12 +6,7 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <img
-        width="100%"
-        height="100%"
-        src="https://wallpaperaccess.com/full/231109.jpg"
-        alt=""
-      />
+      <Featured />
     </div>
   );
 };
